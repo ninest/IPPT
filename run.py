fe8f20ca3fea1d02f8297c6766aa48d2e3460d6b
ninest <parth.kabra@gmail.com>
2020-07-30 01:11:30 +0800

    add station enum

---
+++ run.py
@@ -0,0 +1,3 @@
+from data.run import run_score_table
+
+score_table = run_score_table
--- static.py
+++ static.py
@@ -1,22 +1,34 @@
+from enum import Enum, unique
 from data.situp import situp_score_table
 from data.pushup import pushup_score_table
 from age import get_age_group
 
-# score_table = situp_score_table
-score_table = pushup_score_table
-age = 18
-reps = 35
 
-age_group = get_age_group(age)
+@unique
+class Station(Enum):
+  situp = 0
+  pushup = 1
 
-# get the score set for the age group
-score_set = score_table[age_group]["score_set"]
-top_score_reps = score_table[age_group]["top_score_reps"]
 
-# add zeroes to padd the end
-zeroes = top_score_reps - len(score_set)
-score_set += [0] * zeroes
+def get_score(station: Station, age: int, reps: int):
+  if station == Station.situp:
+    score_table = situp_score_table
+  else:
+    score_table = pushup_score_table
 
-# get the score
-score = score_set[::-1][reps]
-print(score)
+  age = 18
+  reps = 35
+
+  age_group = get_age_group(age)
+
+  # get the score set for the age group
+  score_set = score_table[age_group]["score_set"]
+  top_score_reps = score_table[age_group]["top_score_reps"]
+
+  # add zeroes to padd the end
+  zeroes = top_score_reps - len(score_set)
+  score_set += [0] * zeroes
+
+  # get the score
+  score = score_set[::-1][reps]
+  print(score)
