4c7d3db37eab28e38b1f3925ef87b95e3f7ffce1
ninest <parth.kabra@gmail.com>
2020-07-30 01:29:04 +0800

    testing

--- playground.py
+++ playground.py
@@ -1,7 +1,7 @@
 from static import Station, get_score
 
-s_score = get_score(Station.situp, 18, 40)
-p_score = get_score(Station.pushup, 18, 20)
+s_score = get_score(Station.situp, 41, 23)
+# p_score = get_score(Station.pushup, 41, 14)
 
 print(s_score)
-print(p_score)
+# print(p_score)
