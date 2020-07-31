7fe0e3894d78bd80a7e7059a5478c91e7e6737ab
ninest <parth.kabra@gmail.com>
2020-07-31 21:25:45 +0800

    add run

---
+++ run.py
@@ -0,0 +1,26 @@
+from age import get_age_group
+from data.run import run_times, running_score_table
+
+
+def get_run_score(age, secs):
+  age_group = get_age_group(age)
+
+  # check for max or min run times
+  if secs > run_times[0]:
+    secs = run_times[0]
+  elif secs < run_times[-1]:
+    secs = run_times[-1]
+
+  else:
+    # round up to nearest 10
+    secs = round(secs, -1)
+  
+  # get the position in the array of the timing
+  pos = run_times.index(secs)
+  
+  # get score
+  score = running_score_table[age_group][pos]
+  return score
+
+
+print(get_run_score(18, 740))
