0154116dfcd69f61aeae11e170204c86c76c7610
ninest <parth.kabra@gmail.com>
2020-07-31 20:23:03 +0800

    add static

--- static.py
+++ static.py
@@ -1,12 +1,21 @@
 from enum import Enum, unique
 from age import get_age_group
 
+from data.pushup import pushup_score_table
+from data.situp import situp_score_table
+
 
 @unique
 class Station(Enum):
-  situp = 0
-  pushup = 1
+  pushup = 0
+  situp = 1
 
 
 def get_score(station: Station, age: int, reps: int):
-  pass
+  age_group = get_age_group(age)
+  score_table = pushup_score_table if station == Station.pushup else situp_score_table
+
+  # get the score from age group
+  score = score_table[age_group][reps-1]
+
+  return score
