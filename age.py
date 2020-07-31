46b126481eafb94270311e5d5ef394a4cb66f125
ninest <parth.kabra@gmail.com>
2020-07-31 20:16:48 +0800

    added age

--- age.py
+++ age.py
@@ -1,2 +1,23 @@
+"""
+Each age group is 2 years
+To get from one age group down to the next, you have to minus 3
+because an age group is 2 years inclusive
+"""
+
+
 def get_age_group(age):
-  pass
+  """Get age group
+
+  Done by continuously reducing age and going to previous age group
+  and counting how many times this is done
+
+  Note that since arrays start with 0, to get the human-readable age
+  group, add 1
+  """
+
+  group = 0
+  while (age >= 22):
+    group += 1
+    age -= 3
+
+  return group
