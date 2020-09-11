2aab9b2d5c7b8fb3418b21d11e3cf895846bc58d
ninest <parth.kabra@gmail.com>
2020-09-11 09:25:54 +0800

    Add formatting

--- calculator/ippt.py
+++ calculator/ippt.py
@@ -1,4 +1,3 @@
-
 from static import get_static_score, Station
 from run import get_run_score
 from data.awards import awards
@@ -6,10 +5,19 @@ from data.awards import awards
 
 def get_score(age, pushups, situps, run_secs):
     puhsups_score, pushups_next_score = get_static_score(
-        Station.pushup, age, pushups)
+        Station.pushup,
+        age,
+        pushups,
+    )
     situps_score, situps_next_score = get_static_score(
-        Station.situp, age, situps)
-    run_score, run_next_score = get_run_score(age, run_secs)
+        Station.situp,
+        age,
+        situps,
+    )
+    run_score, run_next_score = get_run_score(
+        age,
+        run_secs,
+    )
 
     total = puhsups_score + situps_score + run_score
 
@@ -20,21 +28,12 @@ def get_score(age, pushups, situps, run_secs):
             result = award
 
     return {
-        "pushups": {
-            "score": puhsups_score,
-            "next": pushups_next_score
-        },
-        "situps": {
-            "score": situps_score,
-            "next": situps_next_score
-        },
-        "run": {
-            "score": run_score,
-            "next": run_next_score
-        },
+        "pushups": {"score": puhsups_score, "next": pushups_next_score},
+        "situps": {"score": situps_score, "next": situps_next_score},
+        "run": {"score": run_score, "next": run_next_score},
         "age": age,
         "total": total,
-        "result": result.to_dict()
+        "result": result.to_dict(),
     }
 
 
