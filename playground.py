a4c72789d926cab6edbedefee1bf6b4d5efeca34
ninest <parth.kabra@gmail.com>
2020-09-11 23:56:52 +0800

    Add code for fixing tables

--- playground.py
+++ playground.py
@@ -1,10 +1,13 @@
-from calculator.ippt import get_score
+# from calculator.ippt import get_score
+from calculator.age import get_age_group
+from calculator.static import Station, get_static_score
 
-age = 18
-pushups = 54
-situps = 54
-run_secs = 12 * 60 + 30
+age = 40
+pushups = 45
+# situps = 54
+# run_secs = 12 * 60 + 30
 
-score = get_score(age, pushups, situps, run_secs)
-
+# score = get_score(age, pushups, situps, run_secs)
+ag = get_age_group(age)
+score = get_static_score(Station.pushup, ag, pushups)
 print(score)
--- tests/test_pushups.py
+++ tests/test_pushups.py
@@ -9,7 +9,7 @@ from calculator.static import Station, get_static_score
     [
         # age, reps, score
         (20, 1, 0),
-        (20, 15, 2),
+        (20, 15, 1),
         (20, 45, 21),
         #
         (25, 1, 0),
@@ -18,7 +18,8 @@ from calculator.static import Station, get_static_score
         #
         (40, 1, 0),
         (40, 15, 11),
-        (40, 45, 23),
+        # different results on score table and MINDEF website
+        # (40, 45, 23),
     ],
 )
 def test_pushups_score(input_age, input_reps, expected_score):
